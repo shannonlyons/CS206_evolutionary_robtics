5ed1dedd14d40916b8294f673f0396c475058476
shannonlyons <59316588+shannonlyons@users.noreply.github.com>
2022-02-07 16:12:06 -0500

    Completed up to step 27 in motors

--- simulate.py
+++ simulate.py
@@ -3,6 +3,8 @@ import numpy as np
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import math
+import random
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -14,17 +16,18 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
 pyrosim.Prepare_To_Simulate(robotId)
-backLegSensorValues = np.zeros(500)
-frontLegSensorValues = np.zeros(500)
-#print(backLegSensorValues)
-for i in range (500):
+backLegSensorValues = np.zeros(1000)
+frontLegSensorValues = np.zeros(1000)
+
+pie = math.pi
+
+for i in range (1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(robotId, "Torso_BackLeg", p.POSITION_CONTROL, 0.0, 500)
-    #print(backLegTouch)
+    pyrosim.Set_Motor_For_Joint(robotId, "Torso_BackLeg", p.POSITION_CONTROL, random.uniform(-pie/2, pie/2), 50)
+    pyrosim.Set_Motor_For_Joint(robotId, "Torso_FrontLeg", p.POSITION_CONTROL, random.uniform(-pie/2, pie/2), 50)
     time.sleep(1/60)
-    #print(i)
 
 # save here
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
