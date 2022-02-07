862a7dc8440e9dfac4f9b29f9b8081d30f4ff6b2
shannonlyons <59316588+shannonlyons@users.noreply.github.com>
2022-02-07 17:00:30 -0500

    Completed up to step 40, issue with step 35 (vector being weird,)

--- simulate.py
+++ simulate.py
@@ -3,7 +3,6 @@ import numpy as np
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
-import math
 import random
 
 physicsClient = p.connect(p.GUI)
@@ -15,19 +14,25 @@ p.loadSDF("world.sdf")
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 
+amplitude = np.pi/4
+frequency = 1
+phaseOffset = 0
+
 pyrosim.Prepare_To_Simulate(robotId)
 backLegSensorValues = np.zeros(1000)
 frontLegSensorValues = np.zeros(1000)
 
-pie = math.pi
+# Left off at step 38
+targetAngles = np.linspace(-np.pi/4, np.pi/4, 1000)
+# np.save('data/targetAngles.npy', targetAngles)
 
 for i in range (1000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(robotId, "Torso_BackLeg", p.POSITION_CONTROL, random.uniform(-pie/2, pie/2), 50)
-    pyrosim.Set_Motor_For_Joint(robotId, "Torso_FrontLeg", p.POSITION_CONTROL, random.uniform(-pie/2, pie/2), 50)
-    time.sleep(1/60)
+    pyrosim.Set_Motor_For_Joint(robotId, "Torso_BackLeg", p.POSITION_CONTROL, targetAngles[i], 20)
+    pyrosim.Set_Motor_For_Joint(robotId, "Torso_FrontLeg", p.POSITION_CONTROL, targetAngles[i], 20)
+    time.sleep(1/240)
 
 # save here
 np.save('data/backLegSensorValues.npy', backLegSensorValues)
